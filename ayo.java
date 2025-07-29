62122bc0582a98ed57919b9580e8f0e0a7ca9a1c
Xandra888 <casey-ann_lumbera@dlsl.edu.ph>
2025-07-30 07:43:37 +0800

    scanner

--- ayo.java
+++ ayo.java
@@ -1,7 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+
 public class ayo {
     public static void main(String[]args){
-        System.out.println("baby");
-        System.out.println("hello");
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
     }
     
 }
