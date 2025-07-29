3f4c34233fc566af3da47996b0f1d84550332533
Xandra888 <casey-ann_lumbera@dlsl.edu.ph>
2025-07-30 07:45:37 +0800

    oops

--- ayo.java
+++ ayo.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class ayo {
     public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
 
