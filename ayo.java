529c57c437d278fb48f4f4e5edda0b66d5a7d4d4
Xandra888 <casey-ann_lumbera@dlsl.edu.ph>
2025-07-29 22:40:40 +0800

    no

--- ayo.java
+++ ayo.java
@@ -1,7 +1,7 @@
 public class ayo {
     public static void main(String[]args){
         System.out.println("baby");
-        System.out.println("duh");
+        System.out.println("d");
     }
     
 }
