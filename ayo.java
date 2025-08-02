5757436a0cd843757e9cdc121403ae05ca2bb497
Xandra888 <casey-ann_lumbera@dlsl.edu.ph>
2025-08-02 09:47:21 +0800

    Update ayo.java

--- ayo.java
+++ ayo.java
@@ -1,16 +1,22 @@
-import java.util.*;
-import java.util.Scanner;
+public class Main{
+    public static void main (String[]args){
 
-public class ayo {
-    public static void main(String[]args){
-        Scanner scanner = new Scanner(System.in);
+        int[][] multiD = new int[2][3];
+        multiD[0][0] = 10;
+        multiD[0][1] = 20;
+        multiD[0][2] = 30;
+       
+        multiD[1][0] = 40;
+        multiD[1][1] = 50;
+        multiD[1][2] = 60;
 
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
-
+        
+        for(int i = 0; i < multiD.length; i++){
+            for(int j = 0; j < multiD[i].length; j++){
+                System.out.println(multiD[i][j]);
+            }
+        }
+        
+        
     }
-    
 }
